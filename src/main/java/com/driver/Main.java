afd08c3d7b57f4e69b2f5869c63dbe94cfb34ebb
Kapil <kapilgargymca@gmail.com>
2024-03-09 19:30:14 +0530

    4th

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,10 @@ package com.driver;
 public class Main {
  public static void main(String args[]){
      Product p=new Product();
-     p.product(10,20);
-     p.product(10,20,30);
-     p.product(10.1,10.2);
+    int ans1= p.product(10,20);
+    int ans2= p.product(10,20,30);
+    double ans3= p.product(10.1,10.2);
+    System.out.print(ans1+" "+ans2+" "+ans3);
     }
 }
 class Product{
